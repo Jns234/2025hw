1b42ee881821025ba232733c63fc963a09eb3694
jan <jan@debian.my-home.net>
2025-08-08 19:56:08 +0300

    add initial tool

---
+++ shellTool/logReadTool.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+LOG_FILE="/home/jan/2025hw/shellTool/logs.log"
+
+LOG_COUNT=$(grep -v '^$' $LOG_FILE | wc -l)
+
+echo "$LOG_COUNT log lines found"
+
+COUNT=1
+## In the example log lines, the last line is not properly terminated, might be part of the task?
+while IFS= read -r line || [ -n "$line" ]; do
+    if [ -z "$line" ]; then
+    continue
+    fi
+    echo "Read line: $COUNT"
+    echo "Sent line: $COUNT"
+    ((COUNT++))
+done < $LOG_FILE
