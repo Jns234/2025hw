37ca94c7de2d1941744c138d73ab094b429c5bcf
jan <jan@debian.my-home.net>
2025-08-13 21:53:21 +0300

    update terraform

--- SIEM/terraform/main.tf
+++ SIEM/terraform/main.tf
@@ -5,6 +5,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.38.1"
     }
+    azapi = {
+      source = "azure/azapi"
+    }
   }
 
   required_version = ">= 1.12.2"
@@ -15,10 +18,16 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
+provider "azapi" {
+  skip_provider_registration = false
+}
+
+
 resource "azurerm_resource_group" "log_ingestion" {
   name     = "log_ingestion_rg"
   location = "North Europe"
 }
+
 
 resource "azurerm_log_analytics_workspace" "log_ingestion_law" {
   name                = "log-ingestion-law"
@@ -28,11 +37,6 @@ resource "azurerm_log_analytics_workspace" "log_ingestion_law" {
   retention_in_days   = 30
 }
 
-resource "azurerm_log_analytics_workspace_table" "log_ingestion_table" {
-  workspace_id            = azurerm_log_analytics_workspace.log_ingestion_law.id
-  name                    = "Ingested_CL"
-  retention_in_days       = 8
-}
 
 resource "azurerm_monitor_data_collection_endpoint" "log_ingestion_mdce" {
   name                          = "log-ingestion-mdce"
@@ -43,22 +47,98 @@ resource "azurerm_monitor_data_collection_endpoint" "log_ingestion_mdce" {
 
 }
 
-#resource "azurerm_monitor_data_collection_rule" "log_ingestion_dcr" {
-#  name                        = "log-ingestion-dcr"
-#  resource_group_name         = azurerm_resource_group.log_ingestion.name
-#  location                    = azurerm_resource_group.log_ingestion.location
-#  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.log_ingestion_mdce.id
-#
-#    destinations {
-#    log_analytics {
-#      workspace_resource_id = azurerm_log_analytics_workspace.log_ingestion_law.id
-#      name                  = "log-ingestion-destination-log"
-#    }
-#  }
-#
-#  data_flow {
-#    streams       = ["Custom-MyTableRawData"]
-#    destinations  = ["log_analytics"]
-#  }
-#
-#}
+resource "azapi_resource" "table" {
+  type      = "Microsoft.OperationalInsights/workspaces/tables@2025-02-01"
+  parent_id = azurerm_log_analytics_workspace.log_ingestion_law.id
+  name      = "Ingested_CL"
+  body = {
+    properties = {
+      plan                 = "Analytics"
+      retentionInDays      = 30
+      totalRetentionInDays = 30
+      schema = {
+        name    = "Ingested_CL"
+        columns = [
+            {
+              name = "host"
+              type = "string"
+            },
+            {
+              name = "ident"
+              type = "string"
+            },
+            {
+              name = "message"
+              type = "string"
+            },
+            {
+              name = "TimeGenerated"
+              type = "datetime"
+            },
+        ]
+      }
+    }
+  }
+  schema_validation_enabled = true
+}
+
+
+resource "azurerm_monitor_data_collection_rule" "log_ingestion_dcr" {
+  name                        = "ingested_dcr"
+  resource_group_name         = azurerm_resource_group.log_ingestion.name
+  location                    = azurerm_resource_group.log_ingestion.location
+  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.log_ingestion_mdce.id
+
+    destinations {
+    log_analytics {
+      workspace_resource_id = azurerm_log_analytics_workspace.log_ingestion_law.id
+      name                  = azurerm_log_analytics_workspace.log_ingestion_law.name
+    }
+  }
+
+  data_flow {
+    built_in_transform = null
+    destinations       = [
+        azurerm_log_analytics_workspace.log_ingestion_law.name
+    ]
+    output_stream      = "Custom-Ingested_CL"
+    streams            = ["Custom-Ingested_CL"]
+    transform_kql      = "source | extend TimeGenerated = now()"
+    }
+
+    stream_declaration {
+        stream_name = "Custom-Ingested_CL"
+
+        column {
+            name = "TimeGenerated"
+            type = "datetime"
+        }
+        column {
+            name = "host"
+            type = "string"
+        }
+        column {
+            name = "ident"
+            type = "string"
+        }
+        column {
+            name = "message"
+            type = "string"
+        }
+        }
+
+}
+
+data "azurerm_subscription" "primary" {
+}
+
+data "azurerm_monitor_data_collection_rule" "log_ingestion_dcr" {
+  name                = "ingested_dcr"
+  resource_group_name = azurerm_resource_group.log_ingestion.name
+}
+
+resource "azurerm_role_assignment" "log_ingestion_access" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Monitoring Metrics Publisher"
+  principal_id         = data.azurerm_monitor_data_collection_rule.log_ingestion_dcr.id
+}
